9a3db7909aca33372c7df1710ceaf0b7bc16143c
ryanwolhuter <dev@ryanwolhuter.com>
2025-11-13 13:31:49 +0200

    step 3 complete - implemented tests for todo operations
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- document-models/todo-list/src/tests/todos.test.ts
+++ document-models/todo-list/src/tests/todos.test.ts
@@ -5,6 +5,11 @@
 
 import { describe, it, expect } from "vitest";
 import { generateMock } from "@powerhousedao/codegen";
+import type {
+  AddTodoItemInput,
+  DeleteTodoItemInput,
+  UpdateTodoItemInput,
+} from "todo-tutorial/document-models/todo-list";
 import {
   reducer,
   utils,
@@ -15,16 +20,17 @@ import {
   UpdateTodoItemInputSchema,
   deleteTodoItem,
   DeleteTodoItemInputSchema,
+  TodoItemSchema,
 } from "todo-tutorial/document-models/todo-list";
 
 describe("Todos Operations", () => {
   it("should handle addTodoItem operation", () => {
     const document = utils.createDocument();
-    const input = generateMock(AddTodoItemInputSchema());
+    const input: AddTodoItemInput = generateMock(AddTodoItemInputSchema());
 
     const updatedDocument = reducer(document, addTodoItem(input));
-
     expect(isTodoListDocument(updatedDocument)).toBe(true);
+
     expect(updatedDocument.operations.global).toHaveLength(1);
     expect(updatedDocument.operations.global[0].action.type).toBe(
       "ADD_TODO_ITEM",
@@ -34,13 +40,24 @@ describe("Todos Operations", () => {
     );
     expect(updatedDocument.operations.global[0].index).toEqual(0);
   });
-  it("should handle updateTodoItem operation", () => {
-    const document = utils.createDocument();
-    const input = generateMock(UpdateTodoItemInputSchema());
+  it("should handle updateTodoItem operation to update text", () => {
+    const mockItem = generateMock(TodoItemSchema());
+    const input: UpdateTodoItemInput = generateMock(
+      UpdateTodoItemInputSchema(),
+    );
+    input.id = mockItem.id;
+    const newText = "new text";
+    input.text = newText;
+    input.checked = undefined;
+    const document = utils.createDocument({
+      global: {
+        items: [mockItem],
+      },
+    });
 
     const updatedDocument = reducer(document, updateTodoItem(input));
-
     expect(isTodoListDocument(updatedDocument)).toBe(true);
+
     expect(updatedDocument.operations.global).toHaveLength(1);
     expect(updatedDocument.operations.global[0].action.type).toBe(
       "UPDATE_TODO_ITEM",
@@ -49,14 +66,58 @@ describe("Todos Operations", () => {
       input,
     );
     expect(updatedDocument.operations.global[0].index).toEqual(0);
+    const updatedItem = updatedDocument.state.global.items.find(
+      (item) => item.id === input.id,
+    );
+    expect(updatedItem?.text).toBe(newText);
+    expect(updatedItem?.checked).toBe(mockItem.checked);
+  });
+  it("should handle updateTodoItem operation to update checked", () => {
+    const mockItem = generateMock(TodoItemSchema());
+    const input: UpdateTodoItemInput = generateMock(
+      UpdateTodoItemInputSchema(),
+    );
+    input.id = mockItem.id;
+    const newChecked = !mockItem.checked;
+    input.checked = newChecked;
+    input.text = undefined;
+    const document = utils.createDocument({
+      global: {
+        items: [mockItem],
+      },
+    });
+
+    const updatedDocument = reducer(document, updateTodoItem(input));
+    expect(isTodoListDocument(updatedDocument)).toBe(true);
+
+    expect(updatedDocument.operations.global).toHaveLength(1);
+    expect(updatedDocument.operations.global[0].action.type).toBe(
+      "UPDATE_TODO_ITEM",
+    );
+    expect(updatedDocument.operations.global[0].action.input).toStrictEqual(
+      input,
+    );
+    expect(updatedDocument.operations.global[0].index).toEqual(0);
+    const updatedItem = updatedDocument.state.global.items.find(
+      (item) => item.id === input.id,
+    );
+    expect(updatedItem?.text).toBe(mockItem.text);
+    expect(updatedItem?.checked).toBe(newChecked);
   });
   it("should handle deleteTodoItem operation", () => {
-    const document = utils.createDocument();
-    const input = generateMock(DeleteTodoItemInputSchema());
-
+    const mockItem = generateMock(TodoItemSchema());
+    const document = utils.createDocument({
+      global: {
+        items: [mockItem],
+      },
+    });
+    const input: DeleteTodoItemInput = generateMock(
+      DeleteTodoItemInputSchema(),
+    );
+    input.id = mockItem.id;
     const updatedDocument = reducer(document, deleteTodoItem(input));
-
     expect(isTodoListDocument(updatedDocument)).toBe(true);
+
     expect(updatedDocument.operations.global).toHaveLength(1);
     expect(updatedDocument.operations.global[0].action.type).toBe(
       "DELETE_TODO_ITEM",
@@ -65,5 +126,7 @@ describe("Todos Operations", () => {
       input,
     );
     expect(updatedDocument.operations.global[0].index).toEqual(0);
+    const updatedItems = updatedDocument.state.global.items;
+    expect(updatedItems).toHaveLength(0);
   });
 });
