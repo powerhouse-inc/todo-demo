7f68fc2a8f938b763f123a5bc45204e7afeebd71
ryanwolhuter <dev@ryanwolhuter.com>
2025-11-13 13:29:54 +0200

    step 2 complete - implemented todo list document model reducer operation handlers
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- document-models/todo-list/src/reducers/todos.ts
+++ document-models/todo-list/src/reducers/todos.ts
@@ -1,16 +1,18 @@
+import { generateId } from "document-model/core";
 import type { TodoListTodosOperations } from "todo-tutorial/document-models/todo-list";
 
 export const todoListTodosOperations: TodoListTodosOperations = {
-    addTodoItemOperation(state, action) {
-        // TODO: Implement "addTodoItemOperation" reducer
-        throw new Error('Reducer "addTodoItemOperation" not yet implemented');
-    },
-    updateTodoItemOperation(state, action) {
-        // TODO: Implement "updateTodoItemOperation" reducer
-        throw new Error('Reducer "updateTodoItemOperation" not yet implemented');
-    },
-    deleteTodoItemOperation(state, action) {
-        // TODO: Implement "deleteTodoItemOperation" reducer
-        throw new Error('Reducer "deleteTodoItemOperation" not yet implemented');
-    }
+  addTodoItemOperation(state, action) {
+    const id = generateId();
+    state.items.push({ ...action.input, id, checked: false });
+  },
+  updateTodoItemOperation(state, action) {
+    const item = state.items.find((item) => item.id === action.input.id);
+    if (!item) return;
+    item.text = action.input.text ?? item.text;
+    item.checked = action.input.checked ?? item.checked;
+  },
+  deleteTodoItemOperation(state, action) {
+    state.items = state.items.filter((item) => item.id !== action.input.id);
+  },
 };
